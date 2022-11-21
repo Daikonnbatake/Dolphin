9e47201bad6a2fd39f1c37de3bd4a2d07a871196
かがまーるまーるまーるまーる <36390388+Daikonnbatake@users.noreply.github.com>
2022-11-21 17:26:42 +0900

    フォーマットのための設定ファイルを追加

--- Dolphin/Application/Application.h
+++ Dolphin/Application/Application.h
@@ -1,39 +1,31 @@
 #pragma once
-#pragma comment(lib, "d2d1" )
-#pragma comment(lib,"windowscodecs.lib")
-#include <windows.h>
-#include <d2d1.h>
-#include <d2d1helper.h>
-#include <dwrite.h>
-#include <wincodec.h>
-#include "Core/Object.h"
-#include "Core/Component.h"
+#include "Dolphin.h"
 
 
 namespace Dolphin
 {
-	class Application;
-	Core::Object* InitObjectTree();
-	void Start(Application& app);
-	void Update(Application& app);
-	void Rendering(Application& app);
-	void Release(Application& app);
+    class Application;
+    Object*       InitObjectTree();
+    void          Start(Application& app);
+    void          Update(Application& app);
+    void          Rendering(Application& app);
+    void          Release(Application& app);
 
-	class Application
-	{
-	private:
-		Core::Object* rootObject;
-		bool quit;
-		bool init;
-		void Start();
+    class Application
+    {
+      private:
+        Core::Object* rootObject;
+        bool          quit;
+        bool          init;
+        void          Start();
 
 
-	public:
-		Application();
-		~Application();
-		Dolphin::Core::Object* Root();
-		void Tick();
-		void Quit();
-		bool IsQuit();
-	};
+      public:
+        Application();
+        ~Application();
+        Dolphin::Core::Object* Root();
+        void                   Tick();
+        void                   Quit();
+        bool                   IsQuit();
+    };
 }
